53ce8b5dfcba56c81fcb83e2b9397e868c3ce373
Rakesh <rakesh.ui@myidos.com>
2024-06-29 12:49:02 +0530

    git

--- Form.js
+++ Form.js
@@ -1,29 +1,41 @@
-// alert("hi")
-// prompt("Hi welcome")
-// confirm("do u want to logout ")
-// document.getElementById('data').innerHTML="HI"
-// var array = [1,2,3,rakesh,lokesh,ravi];
-let array = [1,2,3,4,5,'Rakesh','Lokesh']
-let obj=[
-    {
-        Name : "rakesh",
-        age : 24,
-        gender :'male',
-        graduate : true
-    },
-    {
-        Name : "lokesh",
-        age : 26,
-        gender :'male',
-        graduate : true
-    },
-    {
-        Name : "ravi",
-        age : 29,
-        gender :'male',
-        graduate : true
+setInterval(()=>{
+    var today = new Date();
+    var HRS = today.getHours()
+    if(SEC<10){
+        document.getElementById('hrs').innerHTML= `0${HRS}<sub>hrs</sub>`
+    }else{
+        document.getElementById('hrs').innerHTML= `${HRS}<sub>hrs</sub>`
     }
-]
-var inc = 10
-console.log(++inc)
-console.log(inc++)
+    
+    if(HRS<12){
+    document.getElementById('ampm').innerHTML= 'Am'
+    }else{
+    document.getElementById('ampm').innerHTML= 'Pm'
+    }
+
+    var MIN = today.getMinutes()
+    document.getElementById('min').innerHTML= `${MIN}<sub>mins</sub>`
+
+    var SEC = today.getSeconds()
+    if(SEC<10){
+        document.getElementById('sec').innerHTML= `0${SEC}<sub>s</sub>`
+    }else{
+        document.getElementById('sec').innerHTML= `${SEC}<sub>s</sub>`
+    }
+
+
+    const arrayDay = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+    var DAY = arrayDay[today.getDay()]
+    document.getElementById('day').innerHTML= DAY
+
+    var DT = today.getDate()
+    document.getElementById('date').innerHTML= DT
+
+    const arrayMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var MONTH = arrayMonths[today.getMonth()]
+    document.getElementById('month').innerHTML= MONTH
+
+    const year = today.getFullYear()
+    document.getElementById('year').innerHTML= year
+    console.log(DT)
+},1000);
